b12f75238269b77e8c55bae7036fb63308ad3e65
Diogo Luvizon <diogo@luvizon.com>
2018-10-09 13:42:22 +0200

    Script to download the dataset
    Signed-off-by: Diogo Luvizon <diogo@luvizon.com>

---
+++ dataset/download.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Warning! You are going to dowload 7GB of videos. Ready to continue (y/n)?"
+read opt
+if [ "$opt" != "y" -a "$opt" != "Y" ]
+then
+  exit
+fi
+
+for s in 1 2 3 4 5
+do
+  setdir="set${s}"
+  mkdir ${setdir}
+  wget http://www.dainf.ct.utfpr.edu.br/~rminetto/projects/vehicle-speed/dataset/Set0${s}_video01.h264
+  mv Set0${s}_video01.h264 ${setdir}/video.h264
+  wget http://www.dainf.ct.utfpr.edu.br/~rminetto/projects/vehicle-speed/dataset/Set0${s}_video01.xml
+  mv Set01_video0${s}.xml ${setdir}/ground-truth.xml
+done
+
